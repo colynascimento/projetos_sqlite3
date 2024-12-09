00d9e7df9d847a015656b86efa0d6bf6675fc39c
Coly <colymsnascimento@gmail.com>
2024-12-09 17:36:25 -0300

    atividade crud criacao banco de dados

---
+++ atividades/atividade_crud/criacao_banco_de_dados.py
@@ -0,0 +1,112 @@
+import sqlite3
+
+conn = sqlite3.connect('../projetos_sqlite3/BD/bd_sistema_gerenciamento_passagens.db')
+
+cursor = conn.cursor()
+
+cursor.execute('''       
+    CREATE TABLE IF NOT EXISTS clientes(
+                id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                data_nascimento DATE NOT NULL,
+                nacionalidade TEXT NOT NULL
+                documento TEXT NOT NULL,
+                telefone TEXT NOT NULL,
+                email TEXT NOT NULL,
+                deficiencia_legal TEXT CHECK(deficiencia_legal IN ('sim', 'não'))
+                );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS aeronaves(
+                cod_aeronave INTEGER PRIMARY KEY,
+                modelo TEXT NOT NULL,
+                capacidade_passageiros INTEGER NOT NULL 
+                );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS destinos(
+                cod_iata CHAR(3) PRIMARY KEY,
+                cod_icao CHAR(4) NOT NULL,
+                nome_aeroporto TEXT NOT NULL,
+                cidade TEXT NOT NULL,
+                estado TEXT NOT NULL,
+                pais TEXT NOT NULL,
+                tipo_aeroporto TEXT CHECK(tipo_aeroporto IN ('internacional', 'doméstico', 'regional', 'carga'))     
+                );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS linhas_aereas(
+               cod_linha_aerea INTEGER PRIMARY KEY,
+               cod_iata CHAR(3) NOT NULL,
+               nome TEXT NOT NULL,
+               pais_origem TEXT NOT NULL,
+               contato_suporte TEXT NOT NULL,
+               email TEXT NOT NULL,
+               FOREIGN KEY(cod_iata) REFERENCES destinos(cod_iata)
+               );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS voos(
+               cod_voo INTEGER PRIMARY KEY,
+               cod_linha_aerea INTEGER NOT NULL,
+               cod_aeronave INTEGER NOT NULL,
+               cod_iata_origem INTEGER NOT NULL,
+               cod_iata_destino INTEGER NOT NULL,
+               data_hora_partida TEX NOT NULL,
+               data_hora_chegada TEXT NOT NULL,
+               data_hora_partida TEXT NOT NULL,
+               plataforma INT NOT NULL,
+               FOREIGN KEY(cod_linha_aerea) REFERENCES linhas_aereas(cod_linha_aerea),
+               FOREIGN KEY(cod_aeronave) REFERENCES aeronaves(cod_aeronave),
+               FOREIGN KEY(cod_iata_origem) REFERENCES destinos(cod_iata),
+               FOREIGN KEY(cod_iata_destino) REFERENCES destinos(cod_iata)
+               );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS passagens(
+               cod_passagem INTEGER PRIMARY KEY AUTOINCREMENT,
+               cod_voo INTEGER NOT NULL,
+               id_cliente INTEGER NOT NULL,
+               tipo_assento TEXT CHECK(tipo_assento IN ('executivo', 'comfort', 'primeira classe')),
+               poltrona TEXT NOT NULL,
+               status_passagem TEXT CHECK(status_passagem IN ('emitida', 'reservada', 'cancelada'),
+               preco_final REAL NOT NULL,
+               data_hora_compra TEXT NOT NULL,
+               FOREIGN KEY(cod_voo) REFERENCES voos(cod_voo),
+               FOREIGN KEY(id_cliente) REFERENCES clientes(id_cliente)
+               );
+''')
+
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS preco_base_rota(
+               cod_preco INTEGER PRIMARY KEY AUTOINCREMENT,
+               cod_linha_aerea INTEGER NOT NULL,
+               cod_iata_origem INTEGER NOT NULL,
+               cod_iata_destino INTEGER NOT NULL,
+               preco_base REAL NOT NULL,
+               FOREIGN KEY(cod_linha_aerea) REFERENCES linhas_aereas(cod_linha_aerea),
+               FOREIGN KEY(cod_iata_origem) REFERENCES destinos(cod_iata),
+               FOREIGN KEY(cod_iata_destino) REFERENCES destinos(cod_iata)
+               );
+''')
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS ajustes_preco(
+               cod_ajuste INTEGER PRIMARY KEY AUTOINCREMENT,
+               cod_preco INTEGER NOT NULL,
+               tipo_ajuste TEXT CHECK(tipo_ajuste IN ('desconto', 'aumento'),
+               valor_porcentual REAL NOT NULL,
+               descricao TEXT NOT NULL,
+               data_inicio DATE NOT NULL,
+               data_fim DATE NOT NULL,
+               FOREIGN KEY(cod_preco) REFERENCES preco_base_rota(cod_preco)
+               );
+''')
+conn.commit()
+conn.close()
