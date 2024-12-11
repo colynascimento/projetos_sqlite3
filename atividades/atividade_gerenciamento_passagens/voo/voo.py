a419f07f523acbb9a744c3b9ba71ef2dfba95313
Coly <colymsnascimento@gmail.com>
2024-12-11 17:37:14 -0300

    atividade crud objeto voo

--- atividades/atividade_gerenciamento_passagens/voo/voo.py
+++ atividades/atividade_gerenciamento_passagens/voo/voo.py
@@ -1,1 +1,10 @@
-class 
+class Voo:
+    def __init__(self, cod_voo, cod_linha_aerea, cod_aeronave, cod_iata_origem, cod_iata_destino, data_hora_partida, data_hora_chegada, plataforma):
+        self.cod_voo = cod_voo
+        self.cod_linha_aerea = cod_linha_aerea
+        self.cod_aeronave = cod_aeronave
+        self.cod_iata_origem = cod_iata_origem
+        self.cod_iata_destino = cod_iata_destino
+        self.data_hora_partida = data_hora_partida
+        self.data_hora_chegada = data_hora_chegada
+        self.plataforma = plataforma
