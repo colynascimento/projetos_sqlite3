95f34bf2ac5f602f636b1b836e4fcb4c15c30037
Coly <colymsnascimento@gmail.com>
2024-12-18 17:20:40 -0300

    começando menu

--- atividades/atividade_gerenciamento_passagens/main.py
+++ atividades/atividade_gerenciamento_passagens/main.py
@@ -1,3 +1,23 @@
 import sqlite3
 import os
+import cliente
+import linha_aerea
+import voo
+import sys
+import cliente
+
+os.system('cls')
+
+print('Menu')
+print('_' * 60)
+print('1 - Clientes')
+print('2 - Linhas Aéreas')
+print('3 - Vôos')
+print('_' * 60)
+
+area = input('Qual área deseja acessar? ')
+
+if area == '1':
+    os.system('cls')
+    cliente.cliente_dao.ClienteDao
 
